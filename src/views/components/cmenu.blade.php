0f4b0e317809b45ef584ca8ba62eb5545eb052ab
begicf <fuad.begic@gmail.com>
2021-07-12 10:08:02 +0200

    Update cmenu.blade.php
    bug fix

--- src/views/components/cmenu.blade.php
+++ src/views/components/cmenu.blade.php
@@ -11,14 +11,14 @@
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                     @foreach($item['children'] as $child)
-                        <a class="dropdown-item {{ (request()->is($child->url)) ? 'active' : '' }}" href="{{$child->url}}">{{ $child->title }}</a>
+                        <a class="dropdown-item {{ (request()->is($child->url)) ? 'active' : '' }}" href="/{{$child->url}}">{{ $child->title }}</a>
                     @endforeach
                 </div>
             </li>
 
         @else
 
-            <a class="nav-link {{ (request()->is($item->url)) ? 'active' : '' }}" href="{{$item->url}}">{{ $item->title }}</a>
+            <a class="nav-link {{ (request()->is($item->url)) ? 'active' : '' }}" href="/{{$item->url}}">{{ $item->title }}</a>
 
         @endif
 
